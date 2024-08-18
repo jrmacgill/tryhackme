c29a5a1aa4ecc0fb3bb4dd07b3be657a63757573
jrmacgill <jmacgill@gmail.com>
2024-08-18 22:11:18 +0900

    first

---
+++ hello.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="2487.7">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 19.2px Monaco; color: #558a03; -webkit-text-stroke: #558a03; background-color: #f2efec}
+    span.s1 {font-kerning: none; color: #e88708; -webkit-text-stroke: 0px #e88708}
+    span.s2 {font-kerning: none; color: #000000; -webkit-text-stroke: 0px #000000}
+    span.s3 {font-kerning: none; color: #0c6399; -webkit-text-stroke: 0px #0c6399}
+    span.s4 {font-kerning: none}
+    span.s5 {font-kerning: none; color: #878787; -webkit-text-stroke: 0px #878787}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">&lt;?PHP</span><span class="s2"> </span><span class="s3">echo</span><span class="s2"> </span><span class="s4">"Hello THM"</span><span class="s5">;</span><span class="s2"> </span><span class="s1">?&gt;</span></p>
+</body>
+</html>
